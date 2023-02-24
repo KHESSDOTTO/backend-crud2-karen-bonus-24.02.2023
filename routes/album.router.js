7d9f2fa6f899d3beb5439a7c92ddb1982fc0bb21
Khess S Dotto <khessdotto@gmail.com>
2023-02-24 19:48:30 -0300

    Made alterations on index.js to receive routes. Made the structure to write the routes on album.router.js and purchase.router.js.

---
+++ routes/album.router.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const albumRouter = express.Router();
+
+export default albumRouter;
---
+++ routes/purchase.router.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const purchaseRouter = express.Router();
+
+export default purchaseRouter;
