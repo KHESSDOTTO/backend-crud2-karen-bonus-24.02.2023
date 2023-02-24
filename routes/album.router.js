ae9dd4c63148ee78829535dc7bb507c370544789
Khess S Dotto <khessdotto@gmail.com>
2023-02-24 20:33:34 -0300

    Created empty CRUDs.

--- routes/album.router.js
+++ routes/album.router.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const albumRouter = express.Router();
 
+albumRouter.post("/create", (req, res) => {});
+albumRouter.get("/get/all", (req, res) => {});
+albumRouter.get("/get/:id", (req, res) => {});
+albumRouter.put("edit/:id", (req, res) => {});
+albumRouter.delete("delete/:id", (req, res) => {});
+
 export default albumRouter;
--- routes/purchase.router.js
+++ routes/purchase.router.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const purchaseRouter = express.Router();
 
+purchaseRouter.post("/create", (req, res) => {});
+purchaseRouter.get("/get/all", (req, res) => {});
+purchaseRouter.get("/get/:id", (req, res) => {});
+purchaseRouter.put("edit/:id", (req, res) => {});
+purchaseRouter.delete("delete/:id", (req, res) => {});
+
 export default purchaseRouter;
