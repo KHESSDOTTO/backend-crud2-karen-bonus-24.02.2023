e6f01bafe5eb1d12f0eec357c316a6db621732b0
Khess S Dotto <khessdotto@gmail.com>
2023-02-25 19:49:48 -0300

    Done (albums and purchases).

--- models/purchase.model.js
+++ models/purchase.model.js
@@ -1,8 +1,9 @@
 import { Schema, model } from "mongoose";
+import AlbumModel from "./album.model.js";
 
 const purchaseSchema = new Schema({
   shippingAddress: { type: String, required: true },
-  album: { type: Schema.Types.ObjectId, required: true },
+  album: { type: Schema.Types.ObjectId, required: true, ref: "Albums" },
 });
 
 const PurchaseModel = model("Purchase", purchaseSchema);
--- routes/purchase.router.js
+++ routes/purchase.router.js
@@ -1,12 +1,44 @@
 import express from "express";
+import PurchaseModel from "../models/purchase.model.js";
 import AlbumModel from "../models/album.model.js";
 
 const purchaseRouter = express.Router();
 
-purchaseRouter.post("/create", (req, res) => {});
-purchaseRouter.get("/get/all", (req, res) => {});
-purchaseRouter.get("/get/:id", (req, res) => {});
-purchaseRouter.put("edit/:id", (req, res) => {});
-purchaseRouter.delete("delete/:id", (req, res) => {});
+purchaseRouter.post("/create", async (req, res) => {
+  try {
+    if (req.body.album) {
+      const checkAlbumId = await AlbumModel.findById(req.body.album);
+      if (!checkAlbumId) {
+        return res.status(404).json("Album not found.");
+      }
+    }
+    const newPurchase = await (
+      await PurchaseModel.create(req.body)
+    ).populate("album");
+    return res.status(201).json(newPurchase);
+  } catch (err) {
+    console.log(err);
+    if (err.name.includes("ValidationError")) {
+      return res.status(400).json(err);
+    }
+    return res.status(500).json(err);
+  }
+});
+
+purchaseRouter.get("/get/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const selPurchase = await PurchaseModel.findById(id).populate("album");
+    if (!selPurchase) {
+      return res.status(404).json("Purchase not found");
+    }
+    return res.status(200).json(selPurchase);
+  } catch (err) {
+    console.log(err);
+    if (err.message.includes("type string")) {
+      res.status(400).json(err);
+    }
+  }
+});
 
 export default purchaseRouter;
